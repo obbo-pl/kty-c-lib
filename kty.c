d910a88fc7a3de7eaee36c338ab093796169de05
obbo-pl <47525855+obbo-pl@users.noreply.github.com>
2026-01-12 18:43:45 +0100

    macro reorganization

--- kty.c
+++ kty.c
@@ -14,80 +14,19 @@
 #include <float.h>
 
 
-#ifdef _KTY_DEF_KTY81_210
-#define KTY_DEF_KTY81_210  _KTY_LEN(24)_KTY_DATAPRT(kty81_210)
-#else
-#define KTY_DEF_KTY81_210  /**/
-#endif
-#ifdef _KTY_DEF_KTY81_220
-#define KTY_DEF_KTY81_220  _KTY_LEN(24)_KTY_DATAPRT(kty81_220)
-#else
-#define KTY_DEF_KTY81_220  /**/
-#endif
-#ifdef _KTY_DEF_KTY81_221
-#define KTY_DEF_KTY81_221  _KTY_LEN(24)_KTY_DATAPRT(kty81_221)
-#else
-#define KTY_DEF_KTY81_221  /**/
-#endif
-#ifdef _KTY_DEF_KTY81_222
-#define KTY_DEF_KTY81_222  _KTY_LEN(24)_KTY_DATAPRT(kty81_222)
-#else
-#define KTY_DEF_KTY81_222  /**/
-#endif
-#ifdef _KTY_DEF_KTY81_250
-#define KTY_DEF_KTY81_250  _KTY_LEN(24)_KTY_DATAPRT(kty81_250)
-#else
-#define KTY_DEF_KTY81_250  /**/
-#endif
-#ifdef _KTY_DEF_KTY81_251
-#define KTY_DEF_KTY81_251  _KTY_LEN(24)_KTY_DATAPRT(kty81_251)
-#else
-#define KTY_DEF_KTY81_251  /**/
-#endif
-#ifdef _KTY_DEF_KTY81_252
-#define KTY_DEF_KTY81_252  _KTY_LEN(24)_KTY_DATAPRT(kty81_252)
-#else
-#define KTY_DEF_KTY81_252  /**/
-#endif
-#ifdef _KTY_DEF_KTY84_130
-#define KTY_DEF_KTY84_130  _KTY_LEN(36)_KTY_DATAPRT(kty84_130)
-#else
-#define KTY_DEF_KTY84_130  /**/
-#endif
-#ifdef _KTY_DEF_KTY84_150
-#define KTY_DEF_KTY84_150  _KTY_LEN(36)_KTY_DATAPRT(kty84_150)
-#else
-#define KTY_DEF_KTY84_150  /**/
-#endif
-#ifdef _KTY_DEF_KTY84_151
-#define KTY_DEF_KTY84_151  _KTY_LEN(36)_KTY_DATAPRT(kty84_151)
-#else
-#define KTY_DEF_KTY84_151  /**/
-#endif
-
-
-#define KTY_ITEMS  \
-    KTY_DEF_KTY81_210  \
-    KTY_DEF_KTY81_220  \
-    KTY_DEF_KTY81_221  \
-    KTY_DEF_KTY81_222  \
-    KTY_DEF_KTY81_250  \
-    KTY_DEF_KTY81_251  \
-    KTY_DEF_KTY81_252  \
-    KTY_DEF_KTY84_130  \
-    KTY_DEF_KTY84_150  \
-    KTY_DEF_KTY84_151
-
 
 typedef struct {
     int temperature_c;
     int resistance;
 } kty_couple_t;
 
+
+#undef _KTY_VAR
+#define _KTY_VAR(variant)       /**/
 #undef _KTY_LEN
-#define _KTY_LEN(len)               len
-#undef _KTY_DATAPRT
-#define _KTY_DATAPRT(ptr)           /**/
+#define _KTY_LEN(len)           len
+#undef _KTY_PTR
+#define _KTY_PTR(ptr)           /**/
 
 #if KTY_DEF_KTY81_210 + 0 > 0
 static const kty_couple_t kty81_210[KTY_DEF_KTY81_210] =   {{ -55,   980},
@@ -397,14 +336,14 @@ static const kty_couple_t kty84_151[KTY_DEF_KTY84_151] =   {{ -40,   350},
 
 
 #undef  _KTY_LEN
-#define _KTY_LEN(len)               len,
+#define _KTY_LEN(len)           len,
 static int kty_data_len[] = { KTY_ITEMS };
 
 
 #undef  _KTY_LEN
-#define _KTY_LEN(len)               /**/
-#undef  _KTY_DATAPRT
-#define _KTY_DATAPRT(ptr)           ptr,
+#define _KTY_LEN(len)           /**/
+#undef  _KTY_PTR
+#define _KTY_PTR(ptr)           ptr,
 static const kty_couple_t* const kty_data_ptr[] = { KTY_ITEMS };
 
 
